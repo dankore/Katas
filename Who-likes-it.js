f27e81e1b305b1aa9af89c6d10bf8790d17c1a13
Adamu M. Dankore <adamu.dankore@gmail.com>
2019-10-10 11:15:53 -0500

    Kata done!

---
+++ Who-likes-it.js
@@ -0,0 +1,29 @@
+function likes(names) {
+  // TODO
+  if (!names.length) {
+    return "no one likes this";
+  }
+
+  if (names.length === 1) {
+    return names[0] + " " + "likes this";
+  }
+
+  if (names.length > 1 && names.length <= 2) {
+    return names[0] + " and " + names[1] + " " + "likes this";
+  }
+
+  if (names.length > 2 && names.length <= 3) {
+    return names[0] + ", " + names[1] + " and " + names[2] + " " + "likes this";
+  }
+  if (names.length > 3) {
+    return (
+      names[0] + ", " + names[1] + " and " + (names.length - 2) + " " + "others likes this"
+    );
+  }
+}
+
+console.log(likes([])); // 'no one likes this'
+console.log(likes(["Peter"])); //'Peter likes this'
+console.log(likes(["Jacob", "Alex"])); //'Jacob and Alex like this'
+console.log(likes(["Max", "John", "Mark"])); //'Max, John and Mark like this'
+console.log(likes(["Alex", "Jacob", "Mark", "Max"])); //, 'Alex, Jacob and 2 others like this'
