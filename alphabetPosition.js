21a858603a8527dffb24685894e91f9fc37a217e
Adamu Muhammad Dankore <45286534+dankore@users.noreply.github.com>
2019-10-13 03:53:49 -0500

    Update alphabetPosition.js

--- alphabetPosition.js
+++ alphabetPosition.js
@@ -7,15 +7,16 @@ function alphabetPosition(text) {
         n: 14, o: 15, p: 16, q: 17, r: 18, s: 19, t: 20, u: 21, v: 22, w: 23, x: 24, y: 25, z: 26
     }
      
-    // let spacesApostrophePeriodRemoved = text.replace(/\s+/g, "").replace(/'/g, "").replace(/\./g, "").toLowerCase()
     // Get alphabets without position
     let spacesApostrophePeriodRemoved = text.replace(/[^0-9a-zA-Z]/gi, '').toLowerCase();
     
     let regex = /[a-z]/gi;
+    
     // Match it with given test inputs
     let matched_array = spacesApostrophePeriodRemoved.match(regex)
+    
     // Identify corresponding numbers and return
-    return matched_array ? matched_array.map(letter => alphabetsAndPosition[letter]).join(" ") : null
+    return matched_array ? matched_array.map(letter => alphabetsAndPosition[letter]).join(" ") : ""
 }
-console.log(alphabetPosition("The sunset sets at twelve o' clock.")); //, "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
-console.log(alphabetPosition("}6{>>{01")); //, "20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20"
+console.log(alphabetPosition("The sunset sets at twelve o' clock.")); // "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
+console.log(alphabetPosition("}6{>>{01")); //""
