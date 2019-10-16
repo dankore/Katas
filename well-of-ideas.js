65cd3f6e03bb85c77e019c27092a263f309d1daf
Adamu M. Dankore <adamu.dankore@gmail.com>
2019-10-16 08:39:09 -0500

    Refactor

--- well-of-ideas.js
+++ well-of-ideas.js
@@ -1,22 +1,9 @@
 //Kata: https://www.codewars.com/kata/well-of-ideas-easy-version
 function well(x) {
-  let object_with_count = x.reduce((count, word) => {
-    if (count.hasOwnProperty(word)) {
-      count[word] += 1;
-    } else {
-      count[word] = 1;
-    }
-    return count;
-  }, {});
-
-
-  if (!object_with_count.good) {
-    return "Fail!";
-  } else if (object_with_count.good >= 1 && object_with_count.good <= 2) {
-    return "Publish!";
-  } else {
-    return "I smell a series!";
-  }
+  // Refactor
+  const filtered = x.filter(word => word == "good").length
+  return filtered < 1 ? "Fail!" :
+         filtered < 3 ? "Publish!" : "I smell a series" 
 }
 
 console.log(well(["bad", "bad", "bad"]), "Fail!");
